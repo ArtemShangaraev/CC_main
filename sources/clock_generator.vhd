91ea826be69a00ad368f7de9755547045d62777e
ArtemShangaraev <35298168+ArtemShangaraev@users.noreply.github.com>
2020-03-05 18:48:38 +0300

    Update clock_generator.vhd

--- sources/clock_generator.vhd
+++ sources/clock_generator.vhd
@@ -7,7 +7,8 @@
 -- Company    : CERN
 -- Created    : 2020-02-14
 -- Last update: 2020-02-14
--- Platform   : Cyclone V GX
+-- Platform   : Quartus Prime 18.1.0
+-- Target     : Cyclone V GX
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
 -- Description: PLL with all necessary clocks for the design.
